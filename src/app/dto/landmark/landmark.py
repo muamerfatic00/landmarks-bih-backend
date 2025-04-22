e1999b82ce14490e0f7e5c3a9e5985980d7b5e47
muamer_fatic <muamer.fatic@serapion.net>
2025-04-22 14:59:40 +0200

    Refactored Landmark dto files.

--- src/app/dto/landmark/landmark.py
+++ src/app/dto/landmark/landmark.py
@@ -8,14 +8,25 @@ from src.app.dto.city.city_without_landmark import CityWithoutLandmarkResponse
 class LandmarkBase(CamelModel):
     name: str
     description: str
-
-
-class LandmarkResponse(LandmarkBase):
-    id: int
     image_url: Optional[str]
     google_maps_url: Optional[str]
     contact_number: Optional[str]
     mail: Optional[str]
+
+
+class LandmarkCreateRequest(LandmarkBase):
+    city_id: Optional[int]
+
+
+class LandmarkUpdateRequest(LandmarkBase):
+    city_id: Optional[int]
+
+    class Config:
+        from_attributes = True
+
+
+class LandmarkResponse(LandmarkBase):
+    id: int
     city: Optional[CityWithoutLandmarkResponse]
 
     class Config:
@@ -23,12 +34,13 @@ class LandmarkResponse(LandmarkBase):
         arbitrary_types_allowed = True
 
 
+class LandmarkPostResponse(LandmarkCreateRequest):
+    class Config:
+        from_attributes = True
+
+
 class LandmarkWithoutCityResponse(LandmarkBase):
     id: int
-    image_url: Optional[str]
-    google_maps_url: Optional[str]
-    contact_number: Optional[str]
-    mail: Optional[str]
 
     class Config:
         from_attributes = True
