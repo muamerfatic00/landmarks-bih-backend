f1ae68f980e1a7366195de67597c9fc5d9a7b01c
muamer_fatic <muamer.fatic@serapion.net>
2025-04-22 15:03:30 +0200

    Added create method to BaseService. Added create_landmark, update_landmark to landmark router.

--- src/app/routers/landmark_router.py
+++ src/app/routers/landmark_router.py
@@ -1,13 +1,35 @@
-from fastapi import APIRouter
-from fastapi.params import Depends
+from fastapi import APIRouter, HTTPException
+from fastapi.params import Depends, Query
+from fastapi_pagination import Page
+from pydantic import ValidationError
 from starlette import status
 
-from src.app.dto.landmark.landmark import LandmarkResponse, LandmarkWithoutCityResponse
+from src.app.dto.landmark.landmark import LandmarkResponse, LandmarkUpdateRequest, LandmarkCreateRequest, LandmarkPostResponse
 from src.app.factory.factory import get_base_service
 from src.app.models import Landmark
+from src.app.schemas.pagination import PaginationParam
 from src.app.services.base_service import BaseService
 
 landmark_router = APIRouter(prefix='/landmarks', tags=['landmark'])
+
+
+@landmark_router.post('', response_model=LandmarkPostResponse, status_code=status.HTTP_201_CREATED)
+async def create_landmark(data: LandmarkCreateRequest,
+                          base_service: BaseService = Depends(get_base_service(Landmark))) -> LandmarkPostResponse:
+    return await base_service.create(data, LandmarkPostResponse)
+
+
+@landmark_router.get('/list', response_model=Page[LandmarkResponse], status_code=status.HTTP_200_OK)
+async def get_paginated_landmarks(page: int = Query(default=1), page_size: int = Query(default=10),
+                                  base_service: BaseService = Depends(get_base_service(Landmark))) -> Page[
+    LandmarkResponse]:
+    try:
+        pagination = PaginationParam(page=page, page_size=page_size)
+        return await base_service.get_paginated(pagination, LandmarkResponse)
+    except Exception as e:
+        if isinstance(e, ValidationError):
+            raise HTTPException(status_code=400, detail="Query params page and page_size must be greater than 0!")
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @landmark_router.get('/{_id}', response_model=LandmarkResponse, status_code=status.HTTP_200_OK)
@@ -16,10 +38,16 @@ async def get_landmark_by_id(_id: int,
     return await base_service.get_by_id(_id, LandmarkResponse)
 
 
-@landmark_router.get('', response_model=LandmarkWithoutCityResponse, status_code=status.HTTP_200_OK)
+@landmark_router.get('', response_model=list[LandmarkResponse], status_code=status.HTTP_200_OK)
 async def get_all_landmarks(base_service: BaseService = Depends(get_base_service(Landmark))) -> [
-    list[LandmarkWithoutCityResponse]]:
-    return await base_service.get_all(LandmarkWithoutCityResponse)
+    list[LandmarkResponse]]:
+    return await base_service.get_all(LandmarkResponse)
+
+
+@landmark_router.put('/{_id}', response_model=LandmarkPostResponse, status_code=status.HTTP_200_OK)
+async def update_landmark(_id: int, data: LandmarkUpdateRequest, base_service: BaseService = Depends(
+    get_base_service(Landmark))) -> LandmarkPostResponse:
+    return await base_service.update(_id, data, LandmarkPostResponse)
 
 
 @landmark_router.delete('/{_id}', status_code=status.HTTP_204_NO_CONTENT)
--- src/app/services/base_service.py
+++ src/app/services/base_service.py
@@ -3,7 +3,7 @@ from typing import Optional, Type
 from fastapi import HTTPException
 from fastapi_pagination import Page
 from pydantic import BaseModel
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.exc import NoResultFound, IntegrityError
 from typing_extensions import TypeVar
 
 from src.app.repositories.base_repository import BaseRepository
@@ -17,6 +17,13 @@ RequestModel = TypeVar('RequestModel', bound=BaseModel)
 class BaseService:
     def __init__(self, repository: BaseRepository):
         self.repository = repository
+
+    async def create(self, data: RequestModel, response_model: Type[ResponseModel]) -> Optional[ResponseModel]:
+        try:
+            created_record = await self.repository.create(data.__dict__)
+            return to_dto(response_model, created_record)
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
 
     async def get_all(self, response_model: Type[ResponseModel]) -> Optional[list[ResponseModel]]:
         try:
@@ -37,7 +44,7 @@ class BaseService:
                 raise HTTPException(status_code=404, detail=str(e))
             raise HTTPException(status_code=500, detail=str(e))
 
-    async def get_paginated(self, pagination_param: PaginationParam, response_model: Type[ResponseModel])->Page:
+    async def get_paginated(self, pagination_param: PaginationParam, response_model: Type[ResponseModel]) -> Page:
         try:
             paginated_list = await self.repository.get_paginated(pagination_param, join=True)
             paginated_list.items = [to_dto(response_model, item) for item in paginated_list.items]
@@ -49,7 +56,6 @@ class BaseService:
         ResponseModel]:
         try:
             record_for_update = await self.repository.get_by("id", _id, unique=True)
-            print(record_for_update)
             if not record_for_update:
                 raise NoResultFound(f"Record with id {_id} does not exist.")
             await self.repository.update(record_for_update, data.__dict__)
@@ -57,6 +63,8 @@ class BaseService:
         except Exception as e:
             if isinstance(e, NoResultFound):
                 raise HTTPException(status_code=404, detail=str(e))
+            if isinstance(e, IntegrityError):
+                raise HTTPException(status_code=400, detail=str(e))
             raise HTTPException(status_code=500, detail=str(e))
 
     async def delete_by_id(self, _id: int) -> None:
