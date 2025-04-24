f2ca634b7bf05f61514299ae90838fca54f7c57d
muamer_fatic <muamer.fatic@serapion.net>
2025-04-24 12:06:50 +0200

    Added and implemented accommodation router

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from src.app.routers.accommodation_router import accommodation_router
 from src.app.routers.city_router import city_router
 from src.app.routers.landmark_router import landmark_router
 
@@ -7,6 +8,7 @@ from src.app.routers.landmark_router import landmark_router
 def include_routers(app: FastAPI):
     app.include_router(city_router)
     app.include_router(landmark_router)
+    app.include_router(accommodation_router)
 
 
 def create_app():
